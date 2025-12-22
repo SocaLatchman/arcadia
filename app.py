c232d767ff3129c863cc5008c754f07b1ab123c5
RockyLatchman <r.latchman3624@gmail.com>
2025-12-22 14:52:07 -0800

    Routes, main.css and main.js

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, url_for, render_template
+from flask_wtf.csrf import CSRFProtect
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+csrf_token = CSRFProtect(app)
+
+
+@app.route('/')
+def index():
+    pass
+
+@app.route('/about')
+def about():
+    pass
+
+@app.route('/offerings')
+def offerings():
+    pass
+
+@app.route('/community')
+def community():
+    pass
+
+@app.route('/subscribe')
+def subscribe():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
