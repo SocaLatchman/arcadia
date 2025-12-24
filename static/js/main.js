1c74fc6207c0d7f9cc8485a3b0b731d74bfc5975
RockyLatchman <r.latchman3624@gmail.com>
2025-12-24 00:30:11 -0800

    CSS and js for side panel menu

---
+++ static/js/main.js
@@ -0,0 +1,40 @@
+
+function loadMenu() {
+    const joystick = document.querySelector('#joystick');
+    const sidePanel = document.querySelector('#side-panel');
+    joystick.addEventListener('click', (e) => {
+       e.preventDefault();
+       setTimeout(() => {
+          sidePanel.classList.toggle('is-active');
+       }, 1000) 
+       gsap.to('#joystick-knob', {
+          duration: 0.5,
+          x: 10,
+          y: -1,
+          ease: "elastic.out(1,0.3)"
+       });
+ 
+    })
+    closeMenu();
+}
+
+
+function closeMenu(){
+  const closeButton = document.querySelector('#close-button');
+  const sidePanel = document.querySelector('#side-panel');
+  closeButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      sidePanel.classList.toggle('is-active');
+        gsap.to('#joystick-knob', {
+          duration: 0.5,
+          x: 0,
+          y: 0,
+          ease: "elastic.out(1,0.3)"
+       });
+  })
+}
+
+
+loadMenu();
+
+
